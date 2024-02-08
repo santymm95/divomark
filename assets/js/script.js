e380db96e1d93b1b9849bbf68a8eaaa3383c1829
santymm95 <santymm95@gmail.com>
2024-02-08 15:11:24 -0500

    divomark v1.01

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+/**
+ * header sticky & go to top
+ */
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * navbar toggle
+ */
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navLinks = navbar.querySelectorAll("a");
+
+navToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navToggleBtn);
+  elemToggleFunc(navbar);
+  elemToggleFunc(document.body);
+});
+
+navLinks.forEach(function(navLink) {
+  navLink.addEventListener("click", function() {
+    navbar.classList.remove("active");
+    navToggleBtn.classList.remove("active");
+    document.body.classList.remove("active");
+  });
+});
+
+/**
+ * skills toggle
+ */
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+    elemToggleFunc(skillsBox);
+  });
+}
+
+/**
+ * dark & light theme toggle
+ */
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+
+themeToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(themeToggleBtn);
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.add("dark_theme");
+    document.body.classList.remove("light_theme");
+    localStorage.setItem("theme", "dark_theme");
+  } else {
+   
+
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+    localStorage.setItem("theme", "light_theme");
+  }
+});
+
+/**
+ * check & apply last time selected theme from localStorage
+ */
+if (localStorage.getItem("theme") === "dark_theme") {
+  themeToggleBtn.classList.add("active");
+  document.body.classList.remove("light_theme");
+  document.body.classList.add("dark_theme");
+} else {
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
+}
